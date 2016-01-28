395fab1d0ef82029464daa9b0d301f32537ae79e
idchoi2 <idchoi2@gmail.com>
2016-01-28 19:26:31 +0900

    160128 - Init Commit

---
+++ app.js
@@ -0,0 +1,301 @@
+angular.module('assemblyKioskApp', [])
+    .controller('MainCtrl', ['$scope', '$sce', '$timeout', function($scope, $sce, $timeout) {
+
+        "use strict";
+
+        /**
+         * 초기화
+         */
+        $scope.init = function() {
+            $scope.isQLoad = false;
+            $scope.isALoad = false;
+            $scope.curPage = 1;
+            $scope.curStep = 1;
+            $scope.stepSts = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+            $scope.answer = false;
+            $scope.isFinished = false;
+            $scope.isLoading = false;
+            $scope.isClick = false;
+            $scope.resultNo = 0;
+        };
+
+        /**
+         * 답변 설정
+         * @param answer
+         * @constructor
+         */
+        $scope.SetAnswer = function(answer) {
+            $scope.answer = answer;
+        };
+
+        /**
+         * 페이지 이동
+         * @param paggNo
+         * @param interval
+         * @constructor
+         */
+        $scope.GoPage = function(paggNo, interval) {
+            $timeout(function() {
+                $scope.curPage = paggNo;
+
+                if(paggNo === 2) {
+                    $scope.GoStep($scope.curStep - 1);
+                }
+            }, interval);
+        };
+
+        /**
+         * 단계 이동
+         * @param stepNo
+         * @constructor
+         */
+        $scope.GoStep = function(stepNo) {
+
+            if(stepNo <= $scope.stepSts.length) {
+                $timeout(function() {
+
+                    // 질문 Out
+                    $scope.isQLoad = false;
+
+                    $timeout(function() {
+
+                        // 답변 Out
+                        $scope.isALoad = false;
+
+                        $timeout(function() {
+
+                            // 재설정
+                            if(stepNo > 0) {
+                                if ($scope.answer === "yes") {
+                                    $scope.stepSts[stepNo - 1] = 1;
+                                } else {
+                                    $scope.stepSts[stepNo - 1] = 0;
+                                }
+                            }
+
+                            $scope.answer = false;
+                            $scope.curStep = stepNo + 1;
+                            $scope.isClick = false;
+
+                            if(stepNo < $scope.stepSts.length) {
+                                // 질문 In
+                                $scope.isQLoad = true;
+
+                                $timeout(function() {
+
+                                    // 답변 In
+                                    $scope.isALoad = true;
+
+                                }, 500);
+                            }
+
+                        }, 500);
+                    }, 500);
+                }, 500);
+            }
+
+
+            if(stepNo >= $scope.stepSts.length) {
+                $scope.GoFinish();
+            }
+        };
+
+        /**
+         * 다음 버튼
+         * @constructor
+         */
+        $scope.GoNext = function() {
+            $scope.isClick = true;
+            $scope.GoStep($scope.curStep);
+            //$scope.GoRandom();
+        };
+
+
+        $scope.GoRandom = function() {
+            var fre = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+            var i;
+            for(i = 0; i < 1000; i ++) {
+
+                $scope.stepSts[0] = Math.round(Math.random() * 1);
+                $scope.stepSts[1] = Math.round(Math.random() * 1);
+                $scope.stepSts[2] = Math.round(Math.random() * 1);
+                $scope.stepSts[3] = Math.round(Math.random() * 1);
+                $scope.stepSts[4] = Math.round(Math.random() * 1);
+                $scope.stepSts[5] = Math.round(Math.random() * 1);
+                $scope.stepSts[6] = Math.round(Math.random() * 1);
+                $scope.stepSts[7] = Math.round(Math.random() * 1);
+                $scope.stepSts[8] = Math.round(Math.random() * 1);
+                $scope.stepSts[9] = Math.round(Math.random() * 1);
+                $scope.stepSts[10] = Math.round(Math.random() * 1);
+                $scope.stepSts[11] = Math.round(Math.random() * 1);
+                $scope.stepSts[12] = Math.round(Math.random() * 1);
+                $scope.stepSts[13] = Math.round(Math.random() * 1);
+                $scope.stepSts[14] = Math.round(Math.random() * 1);
+                $scope.stepSts[15] = Math.round(Math.random() * 1);
+
+
+                $scope.GoAnalyze();
+                fre[$scope.resultNo - 1]++;
+            }
+        };
+
+        /**
+         * 분석 하기
+         * @constructor
+         */
+        $scope.GoAnalyze = function() {
+
+            // 2가지 이상 결과
+            if($scope.stepSts[0] !== 1 || $scope.stepSts[1] !== 1) {
+                $scope.stepSts[0] = 0;
+            }
+            $scope.stepSts.splice(1, 1);
+
+            if($scope.stepSts[2] !== 1 || $scope.stepSts[3] !== 1) {
+                $scope.stepSts[2] = 0;
+            }
+            $scope.stepSts.splice(2, 1);
+
+            if($scope.stepSts[4] !== 1 || $scope.stepSts[5] !== 1) {
+                $scope.stepSts[4] = 0;
+            }
+            $scope.stepSts.splice(4, 1);
+
+            if($scope.stepSts[6] !== 1 || $scope.stepSts[7] !== 1) {
+                $scope.stepSts[6] = 0;
+            }
+            $scope.stepSts.splice(6, 1);
+
+
+
+
+
+
+            if($scope.stepSts.equals(result1Cmpr) === true) {
+                $scope.resultNo = 1;
+            } else if($scope.stepSts.equals(result2Cmpr) === true) {
+                $scope.resultNo = 2;
+            } else if($scope.stepSts.equals(result3Cmpr) === true) {
+                $scope.resultNo = 3;
+            } else if($scope.stepSts.equals(result4Cmpr) === true) {
+                $scope.resultNo = 4;
+            } else if($scope.stepSts.equals(result5Cmpr) === true) {
+                $scope.resultNo = 5;
+            } else if($scope.stepSts.equals(result6Cmpr) === true) {
+                $scope.resultNo = 6;
+            } else if($scope.stepSts.equals(result7Cmpr) === true) {
+                $scope.resultNo = 7;
+            } else if($scope.stepSts.equals(result8Cmpr) === true) {
+                $scope.resultNo = 8;
+            } else if($scope.stepSts.equals(result9Cmpr) === true) {
+                $scope.resultNo = 9;
+            } else {
+
+                var dif1 = 0, dif2 = 0, dif3 = 0, dif4 = 0, dif5 = 0, dif6 = 0, dif7 = 0, dif8 = 0, dif9 = 0;
+
+
+
+
+                angular.forEach($scope.stepSts, function(value, key) {
+                    dif1 += (value === 1 && result1[key] === 1) ? 5 : (value === 1 && result1[key] === -1) ? -100 : 0;
+                    dif2 += (value === 1 && result2[key] === 1) ? 5 : (value === 1 && result2[key] === -1) ? -100 : 0;
+                    dif3 += (value === 1 && result3[key] === 1) ? 5 : (value === 1 && result3[key] === -1) ? -100 : 0;
+                    dif4 += (value === 1 && result4[key] === 1) ? 5 : (value === 1 && result4[key] === -1) ? -100 : 0;
+                    dif5 += (value === 1 && result5[key] === 1) ? 5 : (value === 1 && result5[key] === -1) ? -100 : 0;
+                    dif6 += (value === 1 && result6[key] === 1) ? 5 : (value === 1 && result6[key] === -1) ? -100 : 0;
+                    dif7 += (value === 1 && result7[key] === 1) ? 5 : (value === 1 && result7[key] === -1) ? -100 : 0;
+                    dif8 += (value === 1 && result8[key] === 1) ? 5 : (value === 1 && result8[key] === -1) ? -100 : 0;
+                    dif9 += (value === 1 && result9[key] === 1) ? 5 : (value === 1 && result9[key] === -1) ? -100 : 0;
+
+
+                    /*
+                    dif1 += parseInt(Math.abs(value - result1[key]));
+                    dif2 += parseInt(Math.abs(value - result2[key]));
+                    dif3 += parseInt(Math.abs(value - result3[key]));
+                    dif4 += parseInt(Math.abs(value - result4[key]));
+                    dif5 += parseInt(Math.abs(value - result5[key]));
+                    dif6 += parseInt(Math.abs(value - result6[key]));
+                    dif7 += parseInt(Math.abs(value - result7[key]));
+                    dif8 += parseInt(Math.abs(value - result8[key]));
+                    dif9 += parseInt(Math.abs(value - result9[key]));
+                    */
+
+                });
+
+
+
+                var candt = [];
+
+
+                var minV = Math.max((dif1), (dif2), (dif3), (dif4), (dif5), (dif6), (dif7), (dif8), (dif9));
+
+                if(minV === (dif1)) {
+                    candt.push(1);
+                }
+                if(minV === (dif2)) {
+                    candt.push(2);
+                }
+                if(minV === (dif3)) {
+                    candt.push(3);
+                }
+                if(minV === (dif4)) {
+                    candt.push(4);
+                }
+                if(minV === (dif5)) {
+                    candt.push(5);
+                }
+                if(minV === (dif6)) {
+                    candt.push(6);
+                }
+                if(minV === (dif7)) {
+                    candt.push(7);
+                }
+                if(minV === (dif8)) {
+                    candt.push(8);
+                }
+                if(minV === (dif9)) {
+                    candt.push(9);
+                }
+
+                console.log(candt);
+
+                $scope.resultNo = candt[Math.floor((Math.random() * candt.length) + 1) - 1];
+            };
+        };
+
+        /**
+         * 마무리
+         * @constructor
+         */
+        $scope.GoFinish = function() {
+
+            $timeout(function() {
+
+                // 분석중
+                $scope.isLoading = true;
+                $scope.GoAnalyze();
+
+                $timeout(function() {
+
+                    $scope.isLoading = false;
+                    $scope.isFinished = true;
+                    //$scope.stepSts[$scope.stepSts.length - 1] = 1;
+
+                }, 2000);
+
+
+
+            }, 3000);
+        };
+
+        /**
+         * 홈버튼
+         * @constructor
+         */
+        $scope.GoHome = function(isSkip) {
+            if(isSkip || confirm("첫화면으로 이동할까요?")) {
+                $scope.GoPage(1);
+                $scope.init();
+            }
+        };
+    }]);
---
+++ vendor/js/script.js
@@ -0,0 +1,92 @@
+// Warn if overriding existing method
+if(Array.prototype.equals)
+    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+
+
+
+var result1Cmpr = [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+var result2Cmpr = [0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0];
+var result3Cmpr = [1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0];
+var result4Cmpr = [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0];
+var result5Cmpr = [0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0];
+var result6Cmpr = [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0];
+var result7Cmpr = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1];
+var result8Cmpr = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0];
+var result9Cmpr = [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0];
+
+
+var result1 = [0, 1, 1, 0, 0, 0, 0, 0, 0, -1, 0];
+var result2 = [0, 1, 1, 0, 0, 0, 0, 0, 1, 0, -1];
+var result3 = [1, 0, 1, 0, 0, 0, -1, 1, 1, 0, 0];
+var result4 = [1, 0, 1, -1, 0, 0, 0, 0, 1, 0, 0];
+var result5 = [0, 1, 0, 1, 0, 0, 0, -1, 0, 1, 0];
+var result6 = [1, 0, 0, 1, 0, -1, 0, 0, 0, 0, 0];
+var result7 = [0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 1];
+var result8 = [1, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0];
+var result9 = [1, 1, 0, 0, -1, 0, 1, 0, 0, 0, 0];
+
+
+
+
+var timer;
+var timeoutInterval = 60000; // 1000 = 1sec
+
+var assmb = {};
+assmb.autoReset = function() {
+    timer = setTimeout(function(){
+        setTimeout(function() {
+            location.reload();
+        }, 1000);
+    },timeoutInterval);
+};
+
+
+
+
+$(document).bind('contextmenu', function(e) {
+    e.preventDefault();
+    return false;
+});
+$(document).bind("dragstart", function(e) {
+    e.preventDefault();
+    return false;
+});
+$(document).bind("click", function(e) {
+    e.preventDefault();
+    clearTimeout(timer);
+    assmb.autoReset();
+});
+
+$('html').bind("mousewheel DOMMouseScroll", function(e) {
+    e.stopImmediatePropagation();
+    e.stopPropagation();
+    e.preventDefault();
+    return false;
+});
+
